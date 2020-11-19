1fbd81f0a7599e56c7d7b62ad8feaf1992717fc6
William Ard <will42@gmail.com>
2020-11-19 17:45:35 -0600

    updated chess board class and movement script

--- src/chess_board.py
+++ src/chess_board.py
@@ -1,9 +1,14 @@
 import numpy as np 
 
 class chess_board:
-    def __init__(self, sidelength, x0, y0):
+    def __init__(self, x0, y0, x7, y7):
         self.board_state = np.array([[1, 1, 1, 1, 1, 1, 1, 1],[1, 1, 1, 1, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 1, 1, 1, 1],[1, 1, 1, 1, 1, 1, 1, 1]])
-        self.coordinates = np.linspace(x0 + sidelength/8/2, y0 + sidelength - sidelength/8/2, 8)
+        # self.coordinates_x = np.linspace(x0 + sidelength/8/2, x0 - 7*sidelength/8, 8)
+        # self.coordinates_y = np.linspace(y0 + 7*sidelength/8, y0 + sidelength/8/2, 8)
+        self.coordinates_x = np.linspace(x0, x7, 8)
+        self.coordinates_y = np.linspace(y0, y7, 8)
+        print(self.coordinates_x)
+        print(self.coordinates_y)
         # [self.coordinates_x, self.coordinates_y] = np.meshgrid(x0 + self.sidelength_dummy, y0 + self.sidelength_dummy)
 
 
@@ -19,7 +24,7 @@ class chess_board:
 
 
     def get_xy(self, Row, Col):
-        return [self.coordinates[Row], self.coordinates[Col]]
+        return [self.coordinates_x[Col],self.coordinates_y[Row]]
 
 
     def update_board(self, Row1, Col1, Row2, Col2):
@@ -44,3 +49,4 @@ class chess_board:
         self.tuple = self.occupied_flag, self.point1x, self.point1y, self.point2x, self.point2y
         self.string = '(' + ', '.join(self.tuple) + ')'
         return self.string
+ 
--- src/main.py
+++ src/main.py
@@ -4,21 +4,24 @@ from time import sleep
 
 
 # Initialize the socket object for communication with the teach pendant
-HOST = "192.168.0.7" # The remote host
+HOST = "192.168.0.12" # The remote host
 PORT = 8000 # The same port as used by the server
-s = ur_socket_connection.ur_socket_connection(HOST,PORT)
+# s = ur_socket_connection.ur_socket_connection(HOST,PORT)
 
 # Initialize the chessboard. Specify the length of one side of the board.
-# (x0, y0) gives the upper right corner of the board in the UR5 base coordinate system.
+# (x0, y0) gives the upper left corner of the board in the UR5 base coordinate system.
 # Units are in meters.
-cb = chess_board.chess_board(sidelength = 0.4, x0 = -0.6, y0 = -0.4)
+cb = chess_board.chess_board(x0 = 0.330, y0 = -0.8, x7 = 0.010, y7 = -0.430)
 
 
-
+# A8=(0,0)=(y0,x0) ...  ... H8=(0,7)
+#    .                          .
+#    .                          .
+# A1=(7,0)          ...  ... H1=(7,7)=(y7,x7)
 # The generate_movestring function:
 # Converts from Position 1 = Row1, Col1
 #               Position 2 = Row2, Col2
-# Where input = (Row1, Col1, Row2, Col2)
+# Where input = (Row1, Col1, Row2, Col2) (y1,x1,y2,x2)
 # To Cartesian coordinates in meters
 # Also returns an indicator that states if the square that the piece needs
 # to move to is occupied.
@@ -27,7 +30,9 @@ cb = chess_board.chess_board(sidelength = 0.4, x0 = -0.6, y0 = -0.4)
 #
 # output = "(occupied_indicator, x1, y1, x2, y2)"
 
-x=cb.generate_movestring(0,1,0,3)
+x=cb.generate_movestring(1,1,3,1) #In Row,Col notation
 print(x)
-s.pass_msg(x)  # Sends msg to UR5
+# s.pass_msg(x)  # Sends msg to UR5
 
+#330,-800 top left, a8
+#10,-430 bottom right, h1
